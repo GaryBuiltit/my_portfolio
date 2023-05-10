25ff3462b96228c08dcdd6ac43e72fa0c7a69206
GaryBuiltit <alfordg86@gmail.com>
2023-05-10 14:02:54 -0400

    file updates

--- build/src/js/about.js
+++
@@ -1,9 +0,0 @@
-const hamMenuBtn = document.getElementById("menu-btn");
-const hamMenu = document.getElementById("menu");
-const navMenu = document.getElementById("navMenu");
-
-hamMenuBtn.addEventListener("click", () => {
-  hamMenuBtn.classList.toggle("open");
-  hamMenu.classList.toggle("hidden");
-  hamMenu.classList.toggle("flex");
-});
